3526e77c98898e0e5466d5b651678408b5567d0e
faustinegomand <62432521+faustinegomand@users.noreply.github.com>
2020-03-31 17:02:52 +0200

    Automize filament angle calculation
    Angle is caculated in radians. This file needs to be launched after the main file's completion.

---
+++ angleinrad.m
@@ -0,0 +1,7 @@
+%% calculate the angle (Faustine)
+
+for j = 1 : 44
+angle(j) = (xy.spl{j}(length(xy.spl{j}),2)-xy.spl{j}(1,2))/(xy.spl{j}(length(xy.spl{j}),1)-xy.spl{j}(1,1));
+end
+filename = 'angle.xlsx';
+writematrix(angle,filename,'Sheet',1);
