39feb455a666de5bbf8c8158138d9ec462540774
faustinegomand <62432521+faustinegomand@users.noreply.github.com>
2020-04-01 12:07:48 +0200

    Angle given in deg (in addition to rad)

--- additionaldata.m
+++ additionaldata.m
@@ -3,13 +3,14 @@ filename = 'additionaldata.xlsx';
 
 for j = 1 : length(xy.frame)
 angle(j) = (xy.spl{j}(length(xy.spl{j}),2)-xy.spl{j}(1,2))/(xy.spl{j}(length(xy.spl{j}),1)-xy.spl{j}(1,1));
+angleindeg(j) = angle(j) * 180 / pi;
 end
-writematrix(angle,filename,'Sheet',1);
+writematrix(angleindeg,filename,'Sheet',1);
 
 %% calculate the length in microns based on coordinates (Faustine)
 
 for j = 1 : length(xy.frame)
     L(j) = sqrt((xy.spl{j}(length(xy.spl{j}),1)-xy.spl{j}(1,1))^2+(xy.spl{j}(length(xy.spl{j}),2)-xy.spl{j}(1,2))^2);
-    Linmicrons(j) = L(j)/10.24;
+    Linmicrons(j) = L(j)/10.24; %10.24 is the conversion factor pixel/degrees for imgs of 1024x1024 of 100x100microns.
 end
 writematrix(Linmicrons,filename,'Sheet',2);
