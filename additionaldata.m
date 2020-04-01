94aca17d536c4ac66728da8eca543f54e1ff0bc0
faustinegomand <62432521+faustinegomand@users.noreply.github.com>
2020-04-01 15:55:06 +0200

    Centroid crd trajectory (in microns) over frame nb
    3D representation of the centroid trajectory as a function of the frame number (i.e. over time). Trajectory can be given in a 100x100µm space or in a 1024x1024 px space.

--- additionaldata.m
+++ additionaldata.m
@@ -1,5 +1,6 @@
+%% This is Faustine's file
 filename = 'additionaldata.xlsx';
-%% calculate the angle (Faustine)
+%% calculate the angle
 
 for j = 1 : length(xy.frame)
 angle(j) = (xy.spl{j}(length(xy.spl{j}),2)-xy.spl{j}(1,2))/(xy.spl{j}(length(xy.spl{j}),1)-xy.spl{j}(1,1));
@@ -7,10 +8,23 @@ angleindeg(j) = angle(j) * 180 / pi;
 end
 writematrix(angleindeg,filename,'Sheet',1);
 
-%% calculate the length in microns based on coordinates (Faustine)
+%% calculate the length in microns based on coordinates
 
 for j = 1 : length(xy.frame)
     L(j) = sqrt((xy.spl{j}(length(xy.spl{j}),1)-xy.spl{j}(1,1))^2+(xy.spl{j}(length(xy.spl{j}),2)-xy.spl{j}(1,2))^2);
     Linmicrons(j) = L(j)/10.24; %10.24 is the conversion factor pixel/degrees for imgs of 1024x1024 of 100x100microns.
 end
 writematrix(Linmicrons,filename,'Sheet',2);
+
+%% print a 3D plot with the centroid coordinates (x,y) as a function of the frame number
+% trajectory in microns (1024x1024 px -> 100x100 microns)
+
+for j = 1 : length(xy.frame)
+    CENTROID_X(j) = xy.centroid{j}(1,1)*100/1024;
+    CENTROID_Y(j) = xy.centroid{j}(1,2)*100/1024;
+end
+plot3(xy.frame,CENTROID_X,CENTROID_Y);
+%axis equal
+xlabel('Frame nb')
+ylabel('Centroid X')
+zlabel('Centroid Y')
