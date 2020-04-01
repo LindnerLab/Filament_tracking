cb9d0af77919756128247beec62db0b2b7b903e3
faustinegomand <62432521+faustinegomand@users.noreply.github.com>
2020-04-01 12:01:31 +0200

    Delete angleinrad.m

--- angleinrad.m
+++
@@ -1,7 +0,0 @@
-%% calculate the angle (Faustine)
-
-for j = 1 : length(xy.frame)
-angle(j) = (xy.spl{j}(length(xy.spl{j}),2)-xy.spl{j}(1,2))/(xy.spl{j}(length(xy.spl{j}),1)-xy.spl{j}(1,1));
-end
-filename = 'angle.xlsx';
-writematrix(angle,filename,'Sheet',1);
