08cacb1e1abd081409184c8f72afaf3c555861db
faustinegomand <62432521+faustinegomand@users.noreply.github.com>
2020-04-10 17:37:40 +0200

    Av. frame step in sec; tau in sec; ratio tau/tJ
    - Parameters were sorted between Excel sheet 2 (frames and flagellum info) and Sheet 3 (code parameters).
    - Frames parameters include first and last frames, number of frames, frequence, frame step (adimensionned and in seconds, FSav).
    - tau was calculated in seconds assuming that the fit of the autocorrelation takes the number of frames as argument, and multiplying this number by FSav we find out the expofit converted in seconds.
    - ratio tau/tJ was implemented (number of Jeffery oscillations a filament performs before losing memory about its Jeffery orbit state)

--- additionaldata.m
+++ additionaldata.m
@@ -20,24 +20,40 @@
 % ** Autocorrelation function of Cm --> gives a Jeffery decay time tau
 % ** Rotational diffusion time tau_r
 % ** Jeffery oscillation period tJ (depends on the filament aspect ratio and the shear rate)
+% ** Ratio tau/tJ (number of Jeffery oscillations a filament performs before losing memory about its Jeffery orbit state)
 
 %% CODE
 % Name of the file when created (created in the code folder by default)
 filename = 'additionaldata.xlsx';
 
-% INFO ON THE FILAMENT IN MICRONS
-% * equivalent of Jeffery width ("small" radius of the ellipsoid) = helix diameter (between the top of one side of the helix to the other side of the helix)
+% FLAGELLUM INFO
+% * diameter (equivalent of Jeffery width i.e. between the top of one side of the helix to the other side of the helix)
+% * fil_length (length of a straight line going from one end of the filament to the other)
+% * lambda = aspect ratio
 diameter = 0.879;
-% * equivalent of Jeffery length ("long" radius of the ellipsoid) = length of a straight line going from one end of the filament to the other
 fil_length = 8.301;
-% * Aspect ratio
 lambda = fil_length/diameter;
-% * Average length based on arclen in microns (arclenMic)
+% * Average length based on the projected arclen in the xy plane in microns (arclenMic)
 for j = 1 : xy.nframe
-    % Projected arc length Arclen in the xy plane in microns
     arclenMic(j) = xy.arclen(j)*100/1024;
 end
 Lmean = sum(arclenMic)/xy.nframe;
+
+% FRAME INFO
+% * in_frame = First frame (real frame number)
+% * fin_frame = Last frame (real frame number)
+% * step = frame step (a frame is analyzed every ... frames)
+% * total = chosen number of frames (not taking into account missing frames)
+% * F Frequence ("frame per second") in Hz
+% * FS frame step in seconds (time elapsed between two frames)
+% * FSav average frame step in seconds (taking into account missing frames)
+in_frame = xy.frame(1);
+fin_frame = xy.frame(xy.nframe);
+total = (xy.nframe + length(xy.emptyframe));
+step = frame_step;
+F = 30;
+FS = 1/F*step;
+FSav = (total / xy.nframe)*FS;
 
 % INCREMENTAL VARIABLES CALCULATION
 for j = 1 : xy.nframe
@@ -67,18 +83,22 @@ Xcorr = (1:41);
 % * For the fit, Xcorr and Ycorr must be column vectors
 T_Ycorr = transpose(Ycorr);
 T_Xcorr = transpose(Xcorr);
-% Exponential fit for a simple exponential a * exp(b * t) = a * exp(-t/tau)
-% For fits, coordinates must be column vectors
+% Exponential fit for a simple exponential a * exp(b * k) where k is a nb of frames such as t = k*FSav (t=time)
+% Therefore, converting frames in times, the time constant tau such as exp(-t/tau) -> tau = -FSav/b
 expofit = fit(T_Xcorr,T_Ycorr,'exp1');
 plot(expofit,T_Xcorr,T_Ycorr);
 % Harvesting expofit coefficients
 a = expofit.a;
-tau = -1/expofit.b;
+tau = (-FSav/expofit.b);
 
 % JEFFERY OSCILLATION PERIOD
 %!!!! Need to determine gammadot for my experiments
-gammadot = 18; %shear rate (in s-1); 18 s-1 is the value given by Zöttl et al.
-tJ = (2*pi*(lambda + 1/lambda))/gammadot; %Jeffery oscillation period (according to Zöttl et al., 2019)
+% * gammadot is the shear rate (in s-1); 18 s-1 is the value given by Zöttl et al (default value here)
+% * tJ is the Jeffery oscillation period (in s) according to Zöttl et al., 2019
+% * Losingmemory is the number of Jeffery oscillations a filament performs before losing memory about its Jeffery orbit state
+gammadot = 18; 
+tJ = (2*pi*(lambda + 1/lambda))/gammadot;
+Losingmemory = tau/tJ;
 
 % ROTATIONAL DIFFUSION TIME FOR PROLATE BODIES
 % * Boltzmann constant kb in m2 kg s-2 K-1
@@ -103,7 +123,7 @@ Dr = (kb * T) / (6*eta*V*g);
 % Rotation diffusion time
 tau_r = 1/(2*Dr);
 
-% WRITING OUT THE VARIABLE OF INTEREST IN AN EXCEL FILE
+% WRITING OUT THE VARIABLES OF INTEREST IN AN EXCEL FILE
 % * DATA (SHEET 1)
 % 
 % ** Column titles for the Data sheet
@@ -117,6 +137,8 @@ xlswrite(filename,{'Expofit coeff a'},'Feuil1','G1');
 xlswrite(filename,{'Expofit coeff tau'},'Feuil1','H1');
 xlswrite(filename,{'Jeff. period tJ'},'Feuil1','I1');
 xlswrite(filename,{'Rot. diff. time tau_r'},'Feuil1','J1');
+xlswrite(filename,{'Losing memory ratio tau/tJ'},'Feuil1','K1');
+%
 % ** Writing out data by columns on the Excel sheet 1
 % Using transpose() because data are by default organized in rows instead of columns
 writematrix(transpose(Lpinmicrons),filename,'Sheet',1, 'Range', 'A2');
@@ -129,82 +151,81 @@ writematrix(a,filename,'Sheet',1,'Range','G2');
 writematrix(tau,filename,'Sheet',1,'Range','H2');
 writematrix(tJ,filename,'Sheet',1,'Range','I2');
 writematrix(tau_r,filename,'Sheet',1,'Range','J2');
-
+writematrix(Losingmemory,filename,'Sheet',1,'Range','K2');
 %
-% * PARAMETERS (SHEET 2)
+% * FRAMES AND FLAGELLUM (SHEET 2)
 %
-% ** Row titles for the Parameters sheet
+% ** Row titles for the Frames and Flagellum sheet
 xlswrite(filename,{'FRAMES INFO'},'Feuil2','A1');
-xlswrite(filename,{'First frame treated'},'Feuil2','A2');
-xlswrite(filename,{'Frame step'},'Feuil2','A3');
-xlswrite(filename,{'Final frame treated'},'Feuil2','A4');
-xlswrite(filename,{'Nb of treated frames'},'Feuil2','A5');
-xlswrite(filename,{'Nb of empty frames'},'Feuil2','A6');
+xlswrite(filename,{'Frequence (Hz)'},'Feuil2','A2');
+xlswrite(filename,{'Frame step (s)'},'Feuil2','A3');
+xlswrite(filename,{'Average frame step (s)'},'Feuil2','A4');
+xlswrite(filename,{'First frame treated'},'Feuil2','A5');
+xlswrite(filename,{'Final frame treated'},'Feuil2','A6');
+xlswrite(filename,{'Chosen number of frames'},'Feuil2','A7');
+xlswrite(filename,{'Nb of treated frames'},'Feuil2','A8');
+xlswrite(filename,{'Nb of empty frames'},'Feuil2','A9');
+xlswrite(filename,{'FLAGELLUM INFO'},'Feuil2','A10');
+xlswrite(filename,{'Diameter (µm)'},'Feuil2','A11');
+xlswrite(filename,{'Length (µm)'},'Feuil2','A12');
+xlswrite(filename,{'Aspect ratio lambda'},'Feuil2','A13');
+xlswrite(filename,{'Average length Lmean (µm)'},'Feuil2','A14');
 %
-xlswrite(filename,{'FLAGELLUM INFO'},'Feuil2','A7');
-xlswrite(filename,{'Diameter (µm)'},'Feuil2','A8');
-xlswrite(filename,{'Length (µm)'},'Feuil2','A9');
-xlswrite(filename,{'Aspect ratio lambda'},'Feuil2','A10');
-xlswrite(filename,{'Average length Lmean (µm)'},'Feuil2','A11');
+% Writing out data by rows on the Excel sheet 2
+writematrix(F,filename,'Sheet',2,'Range','B2');
+writematrix(FS,filename,'Sheet',2,'Range','B3');
+writematrix(FSav,filename,'Sheet',2,'Range','B4');
+writematrix(in_frame,filename,'Sheet',2,'Range','B5');
+writematrix(fin_frame,filename,'Sheet',2,'Range','B6');
+writematrix(total,filename,'Sheet',2,'Range','B7');
+writematrix(xy.nframe,filename,'Sheet',2,'Range','B8');
+writematrix(length(xy.emptyframe),filename,'Sheet',2,'Range','B9');
 %
-xlswrite(filename,{'CODE PARAMETERS'},'Feuil2','A12');
-xlswrite(filename,{'basepath'},'Feuil2','A13');
-xlswrite(filename,{'tifname'},'Feuil2','A14');
-xlswrite(filename,{'Nb of filaments'},'Feuil2','A15');
-xlswrite(filename,{'Fibermetric thickness (px)'},'Feuil2','A16');
-xlswrite(filename,{'Fibermetric structsensitivity'},'Feuil2','A17');
-xlswrite(filename,{'Gaussian blur noise lengthscale lnois (px)'},'Feuil2','A18');
-xlswrite(filename,{'Gaussian blur object size lobject (px)'},'Feuil2','A19');
-xlswrite(filename,{'Gaussian blur threshold'},'Feuil2','A20');
-xlswrite(filename,{'Binarization sensitivity'},'Feuil2','A21');
-xlswrite(filename,{'Skeletonization MinBranchLength (px)'},'Feuil2','A22');
-xlswrite(filename,{'Bspline ds'},'Feuil2','A23');
-xlswrite(filename,{'Bspline npnts'},'Feuil2','A24');
+writematrix(diameter,filename,'Sheet',2,'Range','B11');
+writematrix(fil_length,filename,'Sheet',2,'Range','B12');
+writematrix(lambda,filename,'Sheet',2,'Range','B13');
+writematrix(Lmean,filename,'Sheet',2,'Range','B14');
 %
-% * Writing out parameters in rows
-% ** First frame
-writematrix(xy.frame(1),filename,'Sheet',2,'Range','B2');
-% ** Frame step chosen
-for j = 1 : xy.nframe
-    if xy.frame(j) == j
-        if ismember(j+1,xy.emptyframe) == 0 %ismember tells if the j+1 element belongs to the xy.frame array
-            writematrix(xy.frame(j+1)-xy.frame(j),filename,'Sheet',2,'Range','B3');
-        end
-    end %!!!! sometimes the loop ends and no value was found for the step if the first if is not verified. Need to improve this.
-end
-% Last frame treated (this number + nb of empty frames = total number of frames asked to treat)
-writematrix(xy.frame(xy.nframe),filename,'Sheet',2,'Range','B4');
-% Nb of frames treated
-writematrix(xy.nframe,filename,'Sheet',2,'Range','B5');
-% Nb of empty frames
-writematrix(length(xy.emptyframe),filename,'Sheet',2,'Range','B6');
+% * CODE PARAMETERS (SHEET 3)
 %
-writematrix(diameter,filename,'Sheet',2,'Range','B8');
-writematrix(fil_length,filename,'Sheet',2,'Range','B9');
-writematrix(lambda,filename,'Sheet',2,'Range','B10');
-writematrix(Lmean,filename,'Sheet',2,'Range','B11');
+% ** Row titles for the Code Parameters sheet
+xlswrite(filename,{'CODE PARAMETERS'},'Feuil3','A1');
+xlswrite(filename,{'basepath'},'Feuil3','A2');
+xlswrite(filename,{'tifname'},'Feuil3','A3');
+xlswrite(filename,{'Nb of filaments'},'Feuil3','A4');
+xlswrite(filename,{'Fibermetric thickness (px)'},'Feuil3','A5');
+xlswrite(filename,{'Fibermetric structsensitivity'},'Feuil3','A6');
+xlswrite(filename,{'Gaussian blur noise lengthscale lnois (px)'},'Feuil3','A7');
+xlswrite(filename,{'Gaussian blur object size lobject (px)'},'Feuil3','A8');
+xlswrite(filename,{'Gaussian blur threshold'},'Feuil3','A9');
+xlswrite(filename,{'Binarization sensitivity'},'Feuil3','A10');
+xlswrite(filename,{'Skeletonization MinBranchLength (px)'},'Feuil3','A11');
+xlswrite(filename,{'Bspline ds'},'Feuil3','A12');
+xlswrite(filename,{'Bspline npnts'},'Feuil3','A13');
 %
-writematrix(basepath,filename,'Sheet',2,'Range','B13');
-writematrix(tifname,filename,'Sheet',2,'Range','B14');
-writematrix(FilNum,filename,'Sheet',2,'Range','B15');
-writematrix(thickness,filename,'Sheet',2,'Range','B16');
-writematrix(structsensitivity,filename,'Sheet',2,'Range','B17');
-writematrix(lnoise,filename,'Sheet',2,'Range','B18');
-writematrix(lobject,filename,'Sheet',2,'Range','B19');
-writematrix(threshold,filename,'Sheet',2,'Range','B20');
-writematrix(sensitivity,filename,'Sheet',2,'Range','B21');
-writematrix(MinBranchLength,filename,'Sheet',2,'Range','B22');
-writematrix(ds,filename,'Sheet',2,'Range','B23');
-writematrix(npnts,filename,'Sheet',2,'Range','B24');
+% Writing out data by rows on the Excel sheet 2
+writematrix(basepath,filename,'Sheet',3,'Range','B2');
+writematrix(tifname,filename,'Sheet',3,'Range','B3');
+writematrix(FilNum,filename,'Sheet',3,'Range','B4');
+writematrix(thickness,filename,'Sheet',3,'Range','B5');
+writematrix(structsensitivity,filename,'Sheet',3,'Range','B6');
+writematrix(lnoise,filename,'Sheet',3,'Range','B7');
+writematrix(lobject,filename,'Sheet',3,'Range','B8');
+writematrix(threshold,filename,'Sheet',3,'Range','B9');
+writematrix(sensitivity,filename,'Sheet',3,'Range','B10');
+writematrix(MinBranchLength,filename,'Sheet',3,'Range','B11');
+writematrix(ds,filename,'Sheet',3,'Range','B12');
+writematrix(npnts,filename,'Sheet',3,'Range','B13');
 %
 % GIVING NAMES TO THE EXCEL SHEETS
 e = actxserver('Excel.Application'); % # open Activex server
 ewb = e.Workbooks.Open('C:\Users\Faustine\Documents\POSTDOC\Image treatment\Francesco - Matlab\Modified code\additionaldata.xlsx'); % # open file (enter full path!)
 ewb.Worksheets.Item(1).Name = 'Data'; % # rename 1st sheet
-ewb.Worksheets.Item(2).Name = 'Parameters'; % # rename 2nd sheet
-ewb.Worksheets.Item(2).Range('A1:B1').Interior.Color=hex2dec('F0F4C3'); % # color row A1
-ewb.Worksheets.Item(2).Range('A7:B7').Interior.Color=hex2dec('F0F4C3'); % # color row A6
-ewb.Worksheets.Item(2).Range('A12:B12').Interior.Color=hex2dec('F0F4C3'); % # color row A11
+ewb.Worksheets.Item(2).Name = 'Frames and Flagellum'; % # rename 2nd sheet
+ewb.Worksheets.Item(3).Name = 'Code Parameters'; % # rename 3rd sheet
+ewb.Worksheets.Item(2).Range('A1:B1').Interior.Color=hex2dec('F0F4C3'); % # color row A1 in sheet 2
+ewb.Worksheets.Item(2).Range('A7:B7').Interior.Color=hex2dec('F0F4C3'); % # color row A6 in sheet 2
+ewb.Worksheets.Item(3).Range('A1:B1').Interior.Color=hex2dec('F0F4C3'); % # color row A1 in sheet 3
 ewb.Save;
 ewb.Close(false)
 e.Quit
